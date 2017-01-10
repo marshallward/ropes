772e72fbe1d61faa78a2fd63e0ce31c1e6ec07a6
Marshall Ward <git@marshallward.org>
2017-01-10 23:04:08 +1100

    slices, __repr__, __str__
    Slice operations were added to the `Rope`.  Slices return a new `Rope`
    using the original rope's nodes.  If the slice partially uses a node's
    data along the ends, then then a new Rope is created for those
    endpoints.
    
    Single-character indexing now returns a single-character `Rope`, rather
    than the single-character string.
    
    Still not sure if I should integrate the single-character indexing into
    the slicing code.  (is there a performance benefit?  Does it matter?)
    
    Finally, `get_string` was rolled into `__str__`, so that `print()` now
    returns the whole string.  `__repr__` was also added, which returns the
    expression for reproducing the `Rope`.
    
    Some work could be done to improve `__repr__`, mostly just removing the
    outer parenthesis, or maybe taking advantage of default associativity
    rules.

--- rope.py
+++ rope.py
@@ -14,22 +14,54 @@ class Rope(object):
         return r
 
     def __getitem__(self, index):
-        if self.left and self.right:    # Checking both is redundant...
-            if index < self.left.length:
-                return self.left[index]
+        # TODO: Combine int and slice?
+        #       Or is it faster to separate them?
+
+        if isinstance(index, int):
+            # NOTE: It should not be necessary to check both
+            if self.left and self.right:
+                if index < self.left.length:
+                    return self.left[index]
+                else:
+                    return self.right[index - self.left.length]
             else:
-                return self.right[index - self.left.length]
-        else:
-            return self.data[index]
+                return Rope(self.data[index])
 
-    def get_string(self):
-        s = ''
+        elif isinstance(index, slice):
+            i, j = index.start, index.stop
+
+            if self.left and self.right:
+                if not i or i < self.left.length:
+                    if j and j <= self.left.length:
+                        return self.left[i:j]
+                    else:
+                        rj = j - self.left.length if j else None
+                        return self.left[i:] + self.right[:rj]
+                else:
+                    rj = j - self.left.length if j else None
+                    return self.right[(i - self.left.length):rj]
+            else:
+                if (not i) and (not j or j == self.length):
+                    return self
+                else:
+                    return Rope(self.data[i:j])
+
+        else:
+            raise TypeError('list indices must be integers or slices, not {}'
+                            ''.format(type(index).__name__))
+
+    def __repr__(self):
+        # TODO: Parentheses are too conservative, need to clean this up
         if self.left and self.right:
-            s = self.left.get_string() + self.right.get_string()
+            return '{}{} + {}{}'.format('(' if self.left else '',
+                                        self.left.__repr__(),
+                                        self.right.__repr__(),
+                                        ')' if self.right else '')
         else:
-            s = self.data
-        return s
+            return "Rope('{}')".format(self.data)
 
-    @property
-    def word(self):
-        return self.get_string()
+    def __str__(self):
+        if self.left and self.right:
+            return self.left.__str__() + self.right.__str__()
+        else:
+            return self.data
